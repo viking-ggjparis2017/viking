e4dfe81fc111b63b1f920e10da046c50f35014bd
ZILLAPORTABLE\Zilla <b.maupied@student.isartdigital.com>
2017-01-21 23:49:22 +0100

    Add frog animation state machine. Change mainMenu.

---
+++ Assets/MainMenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour {
+    private const string GAME_SCENE = "MainScene";
+
+    [SerializeField]
+    TranslationContainer _translationContainer = null;
+
+    [SerializeField]
+    Text _LaunchText, _OptionText, _CreditsText, _ExitText = null;
+
+    // Update is called once per frame
+    public void Update()
+    {
+        _LaunchText.text = _translationContainer.getTranslation("BUTTON_START_GAME");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(MainMenuManager.GAME_SCENE);
+    }
+    
+}
