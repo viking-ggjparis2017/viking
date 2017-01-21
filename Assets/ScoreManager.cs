31a3ab244887bba18d0a258f4f2f36e1a87db0ef
ZILLAPORTABLE\Zilla <b.maupied@student.isartdigital.com>
2017-01-21 01:33:19 +0100

    Update inputs for local multiplayer. Create placeholder assets.

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    int Player_1_Score = 0;
+    int Player_2_Score = 0;
+
+    [SerializeField]
+    Text Player01, Player02;
+
+    void Start () {
+        UpdatePlayerScore();
+    }
+	
+	void Update () {}
+
+    void IncrementPlayer01Score()
+    {
+        ++Player_1_Score;
+        UpdatePlayerScore();
+    }
+
+    void IncrementPlayer02Score()
+    {
+        ++Player_2_Score;
+        UpdatePlayerScore();
+    }
+
+    void UpdatePlayerScore()
+    {
+        Player01.text = "Player 01" + "\n" + "Score: " + Player_1_Score.ToString();
+        Player02.text = "Player 02" + "\n" + "Score: " + Player_2_Score.ToString();
+    }
+
+}
--- Assets/Scripts/PlayerMouvement.cs
+++ Assets/Scripts/PlayerMouvement.cs
@@ -10,11 +10,24 @@ public class PlayerMouvement : MonoBehaviour {
     [SerializeField]
     float mouvementReduction = -10;
 
+    [Range(1, 2), SerializeField]
+    int playerNumber = 1;
+
+    string horizontalAxisName;
+    string verticalAxisName;
+
     Rigidbody rb;
     
 	void Start () {
         rb = GetComponent<Rigidbody>();
-	}
+
+        horizontalAxisName = "Horizontal_P" + playerNumber.ToString(); 
+        verticalAxisName = "Vertical_P" + playerNumber.ToString();
+
+//        print(horizontalAxisName);
+//        print(verticalAxisName);
+
+    }
 	
 	void Update () {}
 
@@ -25,12 +38,14 @@ public class PlayerMouvement : MonoBehaviour {
         var currentVelocity = rb.velocity;
         var oppositeForce = -currentVelocity;
         rb.AddRelativeForce(oppositeForce.x, oppositeForce.y, oppositeForce.z);
-
     }
 
     void Mouvement()
     {
-        rb.AddForce(new Vector3(Input.GetAxis("Horizontal") * mouvementSpeed * Time.deltaTime, 0, Input.GetAxis("Vertical") * mouvementSpeed * Time.deltaTime)
+        rb.AddForce(new Vector3(Input.GetAxis(horizontalAxisName) * mouvementSpeed * Time.deltaTime, 0, Input.GetAxis(verticalAxisName) * mouvementSpeed * Time.deltaTime)
                     , ForceMode.Force);
+
+       // rb.AddForce(new Vector3(Input.GetAxis("Horizontal") * mouvementSpeed * Time.deltaTime, 0, Input.GetAxis("Vertical") * mouvementSpeed * Time.deltaTime)
+         //   , ForceMode.Force);
     }
 }
