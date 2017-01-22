b18460e8a3bc601984387c0b24c9b0442bde5c8a
Guillaume Olivetti <contact@devduweb.com>
2017-01-22 13:47:14 +0100

    music

---
+++ Assets/Scripts/SoundMode.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundMode : MonoBehaviour {
+
+    [SerializeField]
+    private AudioSource sound1;
+    [SerializeField]
+    private AudioSource sound2;
+    [SerializeField]
+    private AudioSource sound3;
+    [SerializeField]
+    private AudioSource victory;
+
+    public void UpdateScore(int score)
+    {
+        if(score > 0 && sound2.mute)
+        {
+            sound2.mute = false;
+        }
+
+        if(score >= 4 && sound3.mute)
+        {
+            sound3.mute = false;
+        }
+    }
+
+    public void GameEnd()
+    {
+        sound1.Stop();
+        sound2.Stop();
+        sound3.Stop();
+        victory.Play();
+    }
+
+    public void ResetGame()
+    {
+        sound1.Play();
+        sound2.Play();
+        sound2.mute = true;
+        sound3.Play();
+        sound2.mute = true;
+        victory.Stop();
+    }
+}
