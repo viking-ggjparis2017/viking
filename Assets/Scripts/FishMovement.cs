05b12cd97adc5f70f9d45b8309b70669b79210fe
Guillaume Olivetti <contact@devduweb.com>
2017-01-22 12:34:03 +0100

    fishes

---
+++ Assets/Scripts/FishMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishMovement : MonoBehaviour {
+    private const int WAIT_MAX = 5;
+    private const float X_MAX = 105f;
+    private const float X_MIN = -103f;
+    private const float Z_MAX = 43f;
+    private const float Z_MIN = -74f;
+
+    [SerializeField]
+    private Transform _selfTransform = null;
+
+    [SerializeField]
+    private Rigidbody _selfRigidBody = null;
+
+    private float _startElevation = 0f;
+    private float _waitTimer = 0f;
+    private Vector3 _destination = Vector3.zero;
+
+	void Start () {
+        _startElevation = _selfTransform.position.y;
+	}
+	
+	void Update () {
+        if(_waitTimer >= 0f)
+        {
+            _waitTimer -= Time.deltaTime;
+            return;
+        }
+
+        Debug.Log("fishmove");
+        GetNewDestination();
+        SetNewTimer();
+        ApplyForce();
+	}
+
+    private void GetNewDestination()
+    {
+        _destination.x = Random.Range(FishMovement.X_MIN, FishMovement.Z_MAX);
+        _destination.z = Random.Range(FishMovement.Z_MIN, FishMovement.Z_MAX);
+        Debug.Log("Dest " + _destination.ToString());
+    }
+
+    private void SetNewTimer()
+    {
+        _waitTimer = Random.Range(0.5f, FishMovement.WAIT_MAX);
+        Debug.Log("wait " + _waitTimer);
+    }
+
+    private void ApplyForce()
+    {
+        Vector3 dir = _destination - _selfTransform.position;
+        Debug.Log("dest " + dir.ToString());
+        _selfRigidBody.AddForce(dir * Random.Range(0.1f, 1f), ForceMode.Impulse);
+    }
+}
