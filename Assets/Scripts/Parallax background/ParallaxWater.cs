232279348e470472b317c3d94f2e3872f190c2b5
ZILLAPORTABLE\Zilla <b.maupied@student.isartdigital.com>
2017-01-22 03:49:36 +0100

    restart parallax effect

---
+++ Assets/Scripts/Parallax background/ParallaxWater.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxWater : MonoBehaviour {
+
+    Ball ball;
+
+    [SerializeField]
+    float _ratio = 1000.0f;
+
+	void Start () {
+        ball = GameObject.FindGameObjectWithTag("Ball").GetComponent<Ball>();
+    }
+
+    void Launch()
+    {
+        var posRatio = (ball.transform.position.x / _ratio) * Time.deltaTime;
+
+        print("pr : " + posRatio);
+        gameObject.GetComponent<Renderer>().material.mainTextureOffset = new Vector2(posRatio, 0);
+    }
+
+    void LateUpdate()
+    {
+        Launch();
+    }
+}
