a4092847976215bcbe3cd29a7570a8b62993b4c8
Unknown <thomas.tillard@laposte.net>
2017-01-22 03:43:35 +0100

    add_fabric_sfx_sound

---
+++ Assets/SoundTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fabric;
+
+public class SoundTest : MonoBehaviour {
+
+// 
+
+	// Use this for initialization
+	void Start () {
+    }
+
+    void TestSon()
+    {
+        if (Input.GetKey("l"))
+            GetComponent<Fabric.AudioComponent>().Play(gameObject);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        TestSon();
+
+    }
+}
